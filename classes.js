4f003560a88a95a12a3fcee11ded592e9ff699e7
user1 <user1@user1s-mbp.attlocal.net>
2018-05-30 22:34:05 -0500

    initial commit

--- classes.js
+++ classes.js
@@ -23,14 +23,26 @@
     - age
   Each employee has the following methods:
     - makeWidget
-      - This returns a string equal to the employees first name + last name + the word widget
+      - This returns a string equal to the employees first name + 
+      last name + the word widget
       - Example: "Dave Smith Widget"
 
-  Call your class Employee and receive all the data in the constructor in the order listed above.
+  Call your class Employee and receive all the data in the constructor 
+  in the order listed above.
 */
 
 //Code Here
-
+class Employee {
+ constructor(first_name,last_name,email,age){
+   this.first_name=first_name;
+   this.last_name=last_name;
+   this.email=email;
+   this.age=age;
+ }
+ makeWidget() {
+  return this.first_name+" "+this.last_name+" Widget";
+}
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -42,14 +54,30 @@
     - reports (other employees) that defaults to an empty array
   Each manager has the following additional methods:
     - hire (employee)
-      - Accepts a new employee as a parameter and pushes it to their list of reports.
+      - Accepts a new employee as a parameter and pushes it to their 
+      list of reports.
     - fire (index)
-      - Fire removes employees from their list of reports at the given index
+      - Fire removes employees from their list of reports at the 
+      given index
 
   Call your new class Manager
 */
 
 //Code Here
+class Manager extends Employee {
+  constructor(first_name,last_name,email,age){
+    super(first_name,last_name,email,age)
+    this.reports=[];
+  }
+  hire(employee){
+    this.reports.push(employee);
+  }
+    
+  fire(index){
+    this.reports.splice(index,1);
+  }
+  }
+var newMan = new Manager('Justin','Timberlake','jt@nfashrtpaowehr.com',77);
 
 
 
@@ -62,7 +90,8 @@
     - title - default 'Not a manager'
     - bonus - default 0
 
-  When employees are added or removed we need to check and update their title. Their titles are as follows:
+  When employees are added or removed we need to check and update their 
+  title. Their titles are as follows:
     0 : Not a manager
     1-3 : Barely Manager
     4-10 : Mostly Manager
@@ -76,7 +105,13 @@
 */
 
 //Code Here
-
+/*class ProgressiveManager {
+  constructor (title,bonus,reports,first_name,last_name,email,age){
+    super(reports,first_name,last_name,email,age)
+    this.title='not a manager'
+    this.bonus=0
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
